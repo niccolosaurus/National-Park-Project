36c4f5a9bdb5af27690f37d6b4a65f3899bc34d5
Daniel Holland <danhol1989@gmail.com>
2022-01-25 22:04:21 -0800

    Squashed commit of the following:
    commit b8ebc2e9bd22fd0aea30de60d3c78dd789731e3e
    Merge: 73000b4 1f0dd55
    Author: Niccolo Eck <92762662+niccolosaurus@users.noreply.github.com>
    Date:   Tue Jan 25 21:56:00 2022 -0800
    
        Merge pull request #3 from Seyaryu/nicco-map-api
    
        Nicco map api
    
    commit 1f0dd55ebda8a8e5d49fa686d0cbcd5de4911baa
    Merge: 9b4871f ea2815a
    Author: Niccolo Eck <nicco.eck@gmail.com>
    Date:   Tue Jan 25 21:53:27 2022 -0800
    
        created an array of objects for the park names and lat/long
    
    commit ea2815addaeb76f23ae78ddcbd00d7ae6007d61d
    Author: Niccolo Eck <nicco.eck@gmail.com>
    Date:   Tue Jan 25 21:45:43 2022 -0800
    
        commiting gitignore
    
    commit 73000b4b93ff4041c0cce6f3cc108f0efa89b79f
    Author: Daniel Holland <danhol1989@gmail.com>
    Date:   Tue Jan 25 21:33:17 2022 -0800
    
        changed script.js
    
    commit 9b4871f69f5b0d60aff80d1d64e1838ea40d01d7
    Author: Niccolo Eck <nicco.eck@gmail.com>
    Date:   Tue Jan 25 21:29:04 2022 -0800
    
        created an array of objects containing the park names, and lat/long
    
    commit 86de08165fda83c442c3bb5100df1f4149846448
    Author: Niccolo Eck <nicco.eck@gmail.com>
    Date:   Tue Jan 25 18:49:14 2022 -0800
    
        updates made
    
    commit 9cd12b2a2939ad71dc9b692718267efbf41f8a64
    Author: Niccolo Eck <nicco.eck@gmail.com>
    Date:   Sun Jan 23 15:45:41 2022 -0800
    
        testing API response

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,39 @@
 var APIkey = 'YzVVecqbLD53XVjKj3RLhIsTHcbfhwuYyq5vgYNI';
-var NPSwebsite = 'https://developer.nps.gov/api/v1/';
+var website = 'developer.nps.gov/api/v1/';
+var APItemplate = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=YzVVecqbLD53XVjKj3RLhIsTHcbfhwuYyq5vgYNI"
 
- var submitSearch = document.querySelector('#submitSearch');
- var parkSearch = document.querySelector('#parkSearch');
+let map;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8, -187.3),
+    mapTypeId: "terrain",
+  });
 
+  // Create a <script> tag and set the USGS URL as the source.
+  const script = document.createElement("script");
 
-initMap();
+  // This example uses a local copy of the GeoJSON stored at
+  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+  script.src =
+    "https://developer.nps.gov/api/v1/parks?limit=500&api_key=YzVVecqbLD53XVjKj3RLhIsTHcbfhwuYyq5vgYNI";
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+const eqfeed_callback = function (results) {
+  for (let i = 0; i < results.features.length; i++) {
+    const coords = results.features[i].geometry.coordinates;
+    const latLong = new google.maps.LatLong(coords[1], coords[0]);
+
+    new google.maps.Marker({
+      position: latLong,
+      map: map,
+    });
+  }
+};
 
 //Use the code below in any function to pull the Nation Parks object.  Function must be "async".
 // var natParks = await pullNPSapi();
@@ -56,19 +83,49 @@ submitSearch.addEventListener('click', function(event){
     
 });
 
-async function initMap() {
-  var options = {
-      zoom:8,
-      center: {lat:32.7157,lng:-117.1611}
-  }
+// async function pullNPSapi() {
+//   var response = await fetch('https://developer.nps.gov/api/v1/parks?limit=500&api_key=YzVVecqbLD53XVjKj3RLhIsTHcbfhwuYyq5vgYNI');
+//   var data = await response.json();
+//   console.log(data);
+//   return data;
+// }
 
-  var map = new google.maps.Map(document.getElementById('map'), options);
+//Use the code below in any function to pull the Nation Parks object.  Function must be "async".
+// var natParks = await pullNPSapi();
+//Fetch National Parks list from
+async function pullNPSapi() {
+  var response = await fetch('https://developer.nps.gov/api/v1/parks?limit=500&api_key=YzVVecqbLD53XVjKj3RLhIsTHcbfhwuYyq5vgYNI');
+  var data = await response.json();
+  console.log(data);
 
-  var natParks = await pullNPSapi();
+return data;
+
 }
 
 
 
+async function natParks(data) {
+// var data = await pullNPSapi();
+var results = []
+for (let i=0; i < data.data.length; i++) {
+  let name = data.data[i].fullName
+  let lat = data.data[i].latitude
+  let long = data.data[i].longitude
+
+  results.push({
+    name, lat, long
+  }); 
+}
+ return results;
+}
+
+async function start() {
+  let data = await pullNPSapi();
+  let response = await natParks(data);
+  console.log(response);
+}
+
+start();
 
 
 
